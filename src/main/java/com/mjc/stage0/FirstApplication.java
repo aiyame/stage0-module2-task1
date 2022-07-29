b09788780c60d3aa53e927a67d199b6e29e83a5c
Aiya <ichanel@mail.ru>
2022-07-29 17:50:31 +0600

    Writing first application

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,6 +2,8 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-        // Write your code here!
+        // Writing first application
+       System.out.println("Hello, World!"); 
+        
     }
 }
